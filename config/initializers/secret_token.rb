6cd99f4358d4530543105f3051d9e495b1fd0363
Nolan Perkins <nolanperk@gmail.com>
2012-05-01 11:34:46 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aysd::Application.config.secret_token = '96adf388f6e70b7f4745656633c7cb099518327eb0a3ddd60e5d5fc7cbf9ec1f6288521507d22d438a692751558082614e33fd4c17606421b13b37bd6da1bf39'
